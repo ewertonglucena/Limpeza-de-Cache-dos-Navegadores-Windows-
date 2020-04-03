9b1d81d127b3ff049d104da46039dfe2b89c370a
Ewerton Gomes de Lucena <44753431+ewertonglucena@users.noreply.github.com>
2020-04-03 13:16:21 -0300

    Add files via upload

---
+++ Limpeza de Cache dos Navegadores (Chrome-IE-Firefox).bat
@@ -0,0 +1,29 @@
+@echo off
+
+taskkill /f /im iexplore.exe
+
+taskkill /f /im chrome.exe
+
+taskkill /f /im firefox.exe
+
+@rem Clear IE cache - (Deletes Temporary Internet Files Only)
+
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+
+erase "%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*.*" /f /s /q
+
+for /D %%i in ("%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*") do RD /S /Q "%%i"
+
+@rem Clear Google Chrome cache
+
+erase "%LOCALAPPDATA%\Google\Chrome\User Data\*.*" /f /s /q
+
+for /D %%i in ("%LOCALAPPDATA%\Google\Chrome\User Data\*") do RD /S /Q "%%i"
+
+@rem Clear Firefox cache
+
+erase "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*.*" /f /s /q
+
+for /D %%i in ("%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*") do RD /S /Q "%%i"
+
+pause?
